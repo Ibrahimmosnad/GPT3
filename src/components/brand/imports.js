c149073d4afeb03027442a44a884b66a21a00d4c
Ibrahim Idris <ibrahimmosnad@gmail.com>
2022-05-27 13:44:07 +0200

    first commit

---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import google from "../../assets/google.png"
+import dropbox from "../../assets/dropbox.png"
+import shopify from "../../assets/shopify.png"
+import slack from "../../assets/slack.png"
+import atlassian from "../../assets/atlassian.png"
+
+export {
+    google,
+    dropbox,
+    shopify,
+    slack,
+    atlassian,
+}
---
+++ src/containers/blog/Blog.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Article } from '../../components';
+import './blog.css';
+import {blog01, blog02, blog03, blog04, blog05} from "./imports"
+
+const Blog = () => {
+  return (
+    <div className="gpt3__blog section__padding" id="blog">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">
+          A lot is happening, We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container__groupA">
+          <Article
+            imageUrl={blog01}
+            date="Sep 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+        </div>
+        <div className="gpt3__blog-container__groupB">
+          <Article
+            imageUrl={blog02}
+            date="Sep 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imageUrl={blog03}
+            date="Sep 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imageUrl={blog04}
+            date="Sep 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imageUrl={blog05}
+            date="Sep 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Blog
